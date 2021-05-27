8c510abbf9310eb1341cca2fc38d50c9425d9cd5
danish <danishjpiet@gmail.com>
2021-05-27 18:09:58 +0530

    rds connected

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'antino_web' );
+define( 'DB_NAME', 'antinodb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Antino$devs567' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'antino-db.capbefndbqax.ap-south-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
