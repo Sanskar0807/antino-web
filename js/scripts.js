34e038c97c55045a9039b6c778d1065ce65958cf
sanskar0807 <sanskar.p@antino.io>
2022-07-20 17:28:26 +0530

    compress images and update form

--- js/scripts.js
+++ js/scripts.js
@@ -534,8 +534,8 @@ function handleTabClick(obj) {
 }
 // MODAL
 function openForm() {
-  
   document.getElementById("myForm").style.display = "flex";
+
 }
 
 function closeForm() {
@@ -554,62 +554,64 @@ function disableSubmitwhileReset4873289000000737242() {
     submitbutton.removeAttribute("disabled");
   }
 }
-// function checkMandatory4873289000000737242() {
-//   var mndFields = new Array("Last Name", "Email");
-//   var fldLangVal = new Array("Name", "Email");
-//   var i;
-//   var mndFieldslength = mndFields.length;
-//   var fieldObj;
-//   for (i = 0; i < mndFieldslength; i++) {
-//     fieldObj =
-//       document.forms.BiginWebToContactForm4873289000000737242[mndFields[i]];
-//     if (fieldObj) {
-//       if (fieldObj.value.replace(/^s+|s+$/g, "").length === 0) {
-//         if (fieldObj.type === "file") {
-//           alert("Please select a file to upload.");
-//           fieldObj.focus();
-//           return false;
-//         }
-//         alert(fldLangVal[i] + " cannot be empty.");
-//         fieldObj.focus();
-//         return false;
-//       } else if (fieldObj.nodeName === "SELECT") {
-//         if (fieldObj.options[fieldObj.selectedIndex].value === "-None-") {
-//           alert(fldLangVal[i] + " cannot be none.");
-//           fieldObj.focus();
-//           return false;
-//         }
-//       } else if (fieldObj.type === "checkbox") {
-//         if (fieldObj.checked === false) {
-//           alert("Please accept " + fldLangVal[i]);
-//           fieldObj.focus();
-//           return false;
-//         }
-//       }
-//       if (fieldObj.name === "Last Name" && fieldObj.value) {
-//         name = fieldObj.value;
-//       }
-//     }
-//   }
-//   return true;
-// }
-// function validateFileUpload() {
-//   var e = document.getElementById("theFile"),
-//     t = 0;
-//   if (e) {
-//     if (e.files.length > 3)
-//       return alert("You can upload a maximum of three files at a time."), !1;
-//     if ("files" in e) {
-//       var i = e.files.length;
-//       if (0 !== i) {
-//         for (var o = 0; o < i; o++) {
-//           var a = e.files[o];
-//           "size" in a && (t += a.size);
-//         }
-//         if (t > 20971520)
-//           return alert("Total file(s) size should not exceed 20MB."), !1;
-//       }
-//     }
-//   }
-//   return !0;
-// }
+
+
+function checkMandatory4873289000000737242() {
+  var mndFields = new Array("Last Name", "Email");
+  var fldLangVal = new Array("Name", "Email");
+  var i;
+  var mndFieldslength = mndFields.length;
+  var fieldObj;
+  for (i = 0; i < mndFieldslength; i++) {
+    fieldObj =
+      document.forms.BiginWebToContactForm4873289000000737242[mndFields[i]];
+    if (fieldObj) {
+      if (fieldObj.value.replace(/^s+|s+$/g, "").length === 0) {
+        if (fieldObj.type === "file") {
+          alert("Please select a file to upload.");
+          fieldObj.focus();
+          return false;
+        }
+        alert(fldLangVal[i] + " cannot be empty.");
+        fieldObj.focus();
+        return false;
+      } else if (fieldObj.nodeName === "SELECT") {
+        if (fieldObj.options[fieldObj.selectedIndex].value === "-None-") {
+          alert(fldLangVal[i] + " cannot be none.");
+          fieldObj.focus();
+          return false;
+        }
+      } else if (fieldObj.type === "checkbox") {
+        if (fieldObj.checked === false) {
+          alert("Please accept " + fldLangVal[i]);
+          fieldObj.focus();
+          return false;
+        }
+      }
+      if (fieldObj.name === "Last Name" && fieldObj.value) {
+        name = fieldObj.value;
+      }
+    }
+  }
+  return true;
+}
+function validateFileUpload() {
+  var e = document.getElementById("theFile"),
+    t = 0;
+  if (e) {
+    if (e.files.length > 3)
+      return alert("You can upload a maximum of three files at a time."), !1;
+    if ("files" in e) {
+      var i = e.files.length;
+      if (0 !== i) {
+        for (var o = 0; o < i; o++) {
+          var a = e.files[o];
+          "size" in a && (t += a.size);
+        }
+        if (t > 20971520)
+          return alert("Total file(s) size should not exceed 20MB."), !1;
+      }
+    }
+  }
+  return !0;
+}
